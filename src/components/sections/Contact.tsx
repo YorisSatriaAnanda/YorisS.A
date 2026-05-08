60acd6251442bf023007e8899635248375665298
YorisSatriaAnanda <artnote33@gmail.com>
2026-05-08 20:08:41 +0700

    mobile again

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
           </div>
           
           <div className="lg:col-span-8">
-            <h3 className="text-4xl sm:text-5xl md:text-7xl font-syne font-bold leading-tight mb-12 uppercase break-words">
+            <h3 className="text-3xl sm:text-5xl md:text-7xl font-syne font-bold leading-tight mb-12 uppercase break-all sm:break-normal">
               Let's create something <span className="text-stroke hover-target text-transparent">unforgettable</span>.
             </h3>
             
